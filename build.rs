bd48ffaae46193b6fe65738432aed2d8e0ab609c
Kornel <kornel@geekhood.net>
2018-12-02 22:57:15 +0000

    Bump

--- build.rs
+++ build.rs
@@ -1,11 +1,10 @@
-extern crate file;
 use std::env;
 
 // env::ARCH doesn't include full triple, and AFAIK there isn't a nicer way of getting the full triple
 // (see lib.rs for the rest of this hack)
 fn main() {
     let out = std::path::PathBuf::from(env::var_os("OUT_DIR").unwrap()).join("default_target.rs");
-    file::put_text(out, env::var("TARGET").unwrap()).unwrap();
+    std::fs::write(out, env::var("TARGET").unwrap()).unwrap();
 
     println!("cargo:rerun-if-changed=build.rs"); // optimization: avoid re-running this script
 }
