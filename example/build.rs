383072901f513246df129df84d6a49052aac9237
Kornel <kornel@geekhood.net>
2019-03-03 20:26:20 +0000

    Make our example test --no-default-features support

--- example/build.rs
+++ example/build.rs
@@ -15,4 +15,11 @@ fn main() {
   if !out_path.exists() { fs::create_dir(&out_path).expect("Could not create assets dir"); }
   File::create(out_path.join("5.txt")).and_then(|mut f| f.write_all(b"Hello generated asset 1")).expect("Could not write asset file");
   File::create(out_path.join("6.txt")).and_then(|mut f| f.write_all(b"Hello generated asset 2")).expect("Could not write asset file");
+
+  if cfg!(feature = "example_non_debian_build") {
+    panic!("Detected this example isn't built via cargo-deb, because example_non_debian_build feature is on. Build with --no-default-features");
+  }
+  if !cfg!(feature = "example_debian_build") {
+    panic!("Detected this example isn't built via cargo-deb, because example_debian_build feature is off. Build with --features=example_debian_build");
+  }
 }
