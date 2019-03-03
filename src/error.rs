d13a8dfed7851cc261acbd596f1443d4deac8e49
Kornel <kornel@geekhood.net>
2019-03-03 20:20:21 +0000

    Remove obsolete error description method

--- src/error.rs
+++ src/error.rs
@@ -12,80 +12,66 @@ quick_error! {
     pub enum CargoDebError {
         Io(err: io::Error) {
             from()
-            description(err.description())
             display("I/O error: {}", err)
             cause(err)
         }
         IoFile(msg: &'static str, err: io::Error, file: PathBuf) {
-            description(msg)
             display("{}: {}", msg, file.display())
             cause(err)
         }
         CommandFailed(err: io::Error, cmd: &'static str) {
-            description(err.description())
             display("Command {} failed to launch", cmd)
             cause(err)
         }
         CommandError(msg: &'static str, arg: String, reason: Vec<u8>) {
-            description(msg)
             display("{} ({}): {}", msg, arg, String::from_utf8_lossy(reason))
         }
         Str(msg: &'static str) {
             from()
-            description(msg)
         }
         NumParse(msg: &'static str, err: num::ParseIntError) {
-            description(msg)
             cause(err)
         }
         InstallFailed {
-            description("dpkg install failed")
+            display("installation failed, because dpkg -i returned error")
         }
         BuildFailed {
-            description("build failed")
+            display("build failed")
         }
         StripFailed(name: PathBuf, reason: String) {
-            description(reason)
             display("unable to strip binary '{}': {}", name.display(), reason)
         }
         SystemTime(err: time::SystemTimeError) {
             from()
-            description("unable to get system time")
+            display("unable to get system time")
             cause(err)
         }
         ParseTOML(err: toml::de::Error) {
             from()
-            description(err.description())
             display("unable to parse Cargo.toml")
             cause(err)
         }
         ParseJSON(err: serde_json::Error) {
             from()
-            description(err.description())
             display("unable to parse `cargo metadata` output")
             cause(err)
         }
         PackageNotFound(path: String, reason: Vec<u8>) {
-            description("unable to find package for the library")
             display("path '{}' does not belong to a package: {}", path, String::from_utf8_lossy(reason))
         }
         VariantNotFound(variant: String) {
-            description("unable to find the passed metadata variant")
             display("[package.metadata.deb.variants.{}] not found in Cargo.toml", variant)
         }
         GlobPatternError(err: glob::PatternError) {
             from()
-            description(err.description())
             display("unable to parse glob pattern")
             cause(err)
         }
         AssetFileNotFound(path: PathBuf) {
-            description("Asset file path does not exist")
             display("Asset file path does not match any files: {}", path.display())
         }
         AssetGlobError(err: glob::GlobError) {
             from()
-            description(err.description())
             display("unable to iterate asset glob result")
             cause(err)
         }
