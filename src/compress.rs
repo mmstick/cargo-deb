b5bafff2c9a4e45b0bdf899b5d7d9f1fe67368e0
Kornel Lesiński <kornel@geekhood.net>
2019-10-02 13:40:56 +0100

    Use fast lzma when --fast is specified

--- src/compress.rs
+++ src/compress.rs
@@ -29,29 +29,21 @@ pub fn gz(data: &[u8]) -> CDResult<Vec<u8>> {
     Ok(compressed)
 }
 
+/// Compresses data using the xz2 library
 #[cfg(feature = "lzma")]
-fn xz(data: &[u8]) -> CDResult<Vec<u8>> {
+pub fn xz_or_gz(data: &[u8], fast: bool) -> CDResult<Compressed> {
     use std::io::Read;
     use xz2::bufread::XzEncoder;
 
     // Compressed data is typically half to a third the original size
     let mut compressed = Vec::with_capacity(data.len() >> 1);
     // Compression level 6 is a good trade off between size and [ridiculously] long compression time
-    XzEncoder::new(data, 6).read_to_end(&mut compressed)?;
+    XzEncoder::new(data, if fast {1} else {6}).read_to_end(&mut compressed)?;
     compressed.shrink_to_fit();
 
-    Ok(compressed)
+    Ok(Compressed::Xz(compressed))
 }
 
-/// Compresses data using the xz2 library
-#[cfg(feature = "lzma")]
-pub fn xz_or_gz(data: &[u8], fast: bool) -> CDResult<Compressed> {
-    if fast {
-        gz(data).map(Compressed::Gz)
-    } else {
-        xz(data).map(Compressed::Xz)
-    }
-}
 
 #[cfg(not(feature = "lzma"))]
 pub fn xz_or_gz(data: &[u8], _fast: bool) -> CDResult<Compressed> {
