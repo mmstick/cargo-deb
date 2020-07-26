579e10c89b060deec05ce8653f501c9eee3a0297
Ximon Eighteen <3304436+ximon18@users.noreply.github.com>
2020-07-26 20:51:44 +0200

    FIX: doc test failures.

--- src/dh_installsystemd.rs
+++ src/dh_installsystemd.rs
@@ -37,11 +37,9 @@ use crate::map;
 ///        debian/package@.target, debian/package.timer, debian/package@.timer
 ///            If any of those files exists, they are installed into
 ///            lib/systemd/system/ in the package build directory.
-/// 
 ///        debian/package.tmpfile
 ///            Only used in compat 12 or earlier.  In compat 13+, this file is
 ///            handled by dh_installtmpfiles(1) instead.
-/// 
 ///            If this exists, it is installed into usr/lib/tmpfiles.d/ in the
 ///            package build directory. Note that the "tmpfiles.d" mechanism is
 ///            currently only used by systemd.
@@ -71,57 +69,57 @@ pub type PackageUnitFiles = HashMap<PathBuf, InstallRecipe>;
 
 /// From man 1 dh_installsystemd on Ubuntu 20.04 LTS. See:
 ///   http://manpages.ubuntu.com/manpages/focal/en/man1/dh_installsystemd.1.html
-/// --no-enable
-/// Disable the service(s) on purge, but do not enable them on install.
-/// 
-/// Note that this option does not affect whether the services are started.  Please
-/// remember to also use --no-start if the service should not be started.
-/// 
-/// --name=name
-/// This option controls several things.
-/// 
-/// It changes the name that dh_installsystemd uses when it looks for maintainer provided
-/// systemd unit files as listed in the "FILES" section.  As an example, dh_installsystemd
-/// --name foo will look for debian/package.foo.service instead of
-/// debian/package.service).  These unit files are installed as name.unit-extension (in
-/// the example, it would be installed as foo.service).
-/// 
-/// Furthermore, if no unit files are passed explicitly as command line arguments,
-/// dh_installsystemd will only act on unit files called name (rather than all unit files
-/// found in the package).
-/// 
-/// --restart-after-upgrade
-/// Do not stop the unit file until after the package upgrade has been completed.  This is
-/// the default behaviour in compat 10.
-/// 
-/// In earlier compat levels the default was to stop the unit file in the prerm, and start
-/// it again in the postinst.
-/// 
-/// This can be useful for daemons that should not have a possibly long downtime during
-/// upgrade. But you should make sure that the daemon will not get confused by the package
-/// being upgraded while it's running before using this option.
-/// 
-/// --no-restart-after-upgrade
-/// Undo a previous --restart-after-upgrade (or the default of compat 10).  If no other
-/// options are given, this will cause the service to be stopped in the prerm script and
-/// started again in the postinst script.
-/// 
-/// -r, --no-stop-on-upgrade, --no-restart-on-upgrade
-/// Do not stop service on upgrade.
-/// 
-/// --no-start
-/// Do not start the unit file after upgrades and after initial installation (the latter
-/// is only relevant for services without a corresponding init script).
-/// 
-/// Note that this option does not affect whether the services are enabled.  Please
-/// remember to also use --no-enable if the services should not be enabled.
-/// 
-/// unit file ...
-/// Only process and generate maintscripts for the installed unit files with the
-/// (base)name unit file.
-/// 
-/// Note: dh_installsystemd will still install unit files from debian/ but it will not
-/// generate any maintscripts for them unless they are explicitly listed in unit file ...
+/// > --no-enable
+/// > Disable the service(s) on purge, but do not enable them on install.
+/// > 
+/// > Note that this option does not affect whether the services are started.  Please
+/// > remember to also use --no-start if the service should not be started.
+/// > 
+/// > --name=name
+/// > This option controls several things.
+/// > 
+/// > It changes the name that dh_installsystemd uses when it looks for maintainer provided
+/// > systemd unit files as listed in the "FILES" section.  As an example, dh_installsystemd
+/// > --name foo will look for debian/package.foo.service instead of
+/// > debian/package.service).  These unit files are installed as name.unit-extension (in
+/// > the example, it would be installed as foo.service).
+/// > 
+/// > Furthermore, if no unit files are passed explicitly as command line arguments,
+/// > dh_installsystemd will only act on unit files called name (rather than all unit files
+/// > found in the package).
+/// > 
+/// > --restart-after-upgrade
+/// > Do not stop the unit file until after the package upgrade has been completed.  This is
+/// > the default behaviour in compat 10.
+/// > 
+/// > In earlier compat levels the default was to stop the unit file in the prerm, and start
+/// > it again in the postinst.
+/// > 
+/// > This can be useful for daemons that should not have a possibly long downtime during
+/// > upgrade. But you should make sure that the daemon will not get confused by the package
+/// > being upgraded while it's running before using this option.
+/// > 
+/// > --no-restart-after-upgrade
+/// > Undo a previous --restart-after-upgrade (or the default of compat 10).  If no other
+/// > options are given, this will cause the service to be stopped in the prerm script and
+/// > started again in the postinst script.
+/// > 
+/// > -r, --no-stop-on-upgrade, --no-restart-on-upgrade
+/// > Do not stop service on upgrade.
+/// > 
+/// > --no-start
+/// > Do not start the unit file after upgrades and after initial installation (the latter
+/// > is only relevant for services without a corresponding init script).
+/// > 
+/// > Note that this option does not affect whether the services are enabled.  Please
+/// > remember to also use --no-enable if the services should not be enabled.
+/// > 
+/// > unit file ...
+/// > Only process and generate maintscripts for the installed unit files with the
+/// > (base)name unit file.
+/// > 
+/// > Note: dh_installsystemd will still install unit files from debian/ but it will not
+/// > generate any maintscripts for them unless they are explicitly listed in unit file ...
 #[derive(Default, Debug)]
 pub struct Options {
     pub no_enable: bool,
